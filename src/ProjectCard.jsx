48ae70d6d2db3a8cc99cac863719b27c124503a1
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-03 23:18:01 +0530

    projects card styles

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,41 +1,57 @@
 import { BiChevronsRight } from 'react-icons/bi';
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 
 const ProjectCard = ({ image, title, description, live, source, tech }) => {
   return (
     <article className="bg-white shadow-lg hover:shadow-xl rounded-lg p-2 w-full flex flex-col gap-2 lg:flex-row lg:gap-8">
-      <img
-        src={image}
-        alt={title}
-        className="border border-green-400 block w-full lg:w-4/12 rounded"
-      />
-      <div className="w-full border border-black">
-        <h3 className="text-lg">{title}</h3>
-        <div>
+      <img src={image} alt={title} className="block w-full lg:w-4/12 rounded" />
+      <div className="w-full">
+        <h3 className="text-xl uppercase px-2 mb-2 tracking-wider font-bold text-slate-800">
+          {title}
+        </h3>
+        <div className="px-4 mb-4">
           {description.map((item, index) => {
             return (
-              <span
-                className="flex items-center gap-2 text-slate-600"
+              <p
+                className="items-center gap-2 text-slate-600 text-md tracking-normal "
                 key={index}
               >
-                <BiChevronsRight /> {item}
-              </span>
+                <BiChevronsRight className="inline mr-2 text-slate-800" />
+                {item}
+              </p>
             );
           })}
         </div>
-        <p>
-          Built with:
+        <p className="px-2 mb-2 tracking-wide">
+          <span className="font-bold text-slate-800">Built with:</span>
           {tech.map((item, index) => (
-            <span key={index}>
+            <span key={index} className="capitalize ml-1">
               {index < tech.length - 1 ? ` ${item}, ` : ` ${item}`}
             </span>
           ))}
         </p>
-        <div>
-          <button>
-            <a href={live}>live app</a>
+        <div className="px-2 mt-4 flex gap-4 font-semibold text-purple-500 ">
+          <button className="btn">
+            <a
+              href={live}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link"
+            >
+              <FaExternalLinkAlt className="icon" />
+              live app
+            </a>
           </button>
-          <button>
-            <a href={source}>source code</a>
+          <button className="btn">
+            <a
+              href={source}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link"
+            >
+              <FaGithub className="icon" />
+              source code
+            </a>
           </button>
         </div>
       </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,7 +3,7 @@ import { projectsData as data } from './utils/data';
 
 const Projects = () => {
   return (
-    <section className="px-20 py-10 grid gap-8 content-normal justify-content-center">
+    <section className="p-4 grid gap-8 content-normal justify-content-center lg:px-10 lg:py-5 ">
       {data.map((item, index) => {
         return <ProjectCard key={index} {...item} />;
       })}
