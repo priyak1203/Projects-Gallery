911163e5395dc19dd8d25778813b056b61f1f885
Priydarshini Karanth <kpriya1203@gmail.com>
2024-04-11 20:24:29 +0530

    toggle button functionality

--- src/Topbar.jsx
+++ src/Topbar.jsx
@@ -1,10 +1,22 @@
+import { BsList, BsFillGridFill } from 'react-icons/bs';
+import { useGlobalContext } from './context';
+
 const Topbar = () => {
+  const { listView, toggleView } = useGlobalContext();
+
   return (
     <header className="bg-sky-700 sticky top-0">
-      <div className="max-w-6xl py-3 mx-auto flex justify-center ">
+      <div className="max-w-6xl py-3 mx-auto flex justify-between ">
         <h1 className="text-3xl font-medium tracking-wide text-white opacity-80">
           Projects Gallery
         </h1>
+        <button onClick={toggleView}>
+          {listView ? (
+            <BsFillGridFill className="topbar-icon" />
+          ) : (
+            <BsList className="topbar-icon text-2xl" />
+          )}
+        </button>
       </div>
     </header>
   );
