05734459e178a4ed9169b372ae514244f9e0a555
Priydarshini Karanth <kpriya1203@gmail.com>
2024-04-13 16:53:38 +0530

    setting up data for modal in context

--- src/context.jsx
+++ src/context.jsx
@@ -1,16 +1,44 @@
 import { createContext, useContext, useState } from 'react';
+import { useFetchProjects } from './fetchProjects';
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [listView, setListView] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalData, setModalData] = useState({});
+
+  const { projects } = useFetchProjects();
+
+  const filterModalData = (id) => {
+    const data = projects.find((item) => item.id === id);
+    setModalData(data);
+  };
 
   const toggleView = () => {
     setListView(!listView);
   };
 
+  const openModal = (id) => {
+    filterModalData(id);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <AppContext.Provider value={{ listView, toggleView }}>
+    <AppContext.Provider
+      value={{
+        listView,
+        toggleView,
+        isModalOpen,
+        openModal,
+        closeModal,
+        modalData,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
